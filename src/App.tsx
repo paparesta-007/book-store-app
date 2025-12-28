759969456df5e3d9d6f62234109cb2e526f91f70
Tommaso <t.paparesta.3231@vallauri.edu>
2025-12-28 22:38:15 +0100

    Aggiunto layout responsive navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,37 @@
-
 import './App.css'
 import UserContext from './context/UserContext'
 import { useState } from 'react'
 import type { User } from "firebase/auth";
 import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-
   const [user, setUser] = useState<User | null>(null);
-  const login = (userData: User) => {
-    setUser(userData);
-  };
 
-  const logout = () => {
-    setUser(null);
-  };
+  const login = (userData: User) => setUser(userData);
+  const logout = () => setUser(null);
+
   return (
-    <>
-      <UserContext.Provider value={{ user, login, logout }}>
+    <UserContext.Provider value={{ user, login, logout }}>
+      <Router>
         <div className="min-h-screen flex flex-col sm:flex-row">
           <Navbar />
 
-          <main className="flex-1 p-4 flex items-center justify-center bg-(--bg-primary)">
-            Contents go here.
+          <main className="flex-1 p-4 flex items-center justify-center bg-[var(--bg-primary)]">
+            <Routes>
+              <Route path="/" element={<h2 className="text-2xl">Discover Page</h2>} />
+              <Route path="/my-space" element={<h2 className="text-2xl">My Space Page</h2>} />
+              <Route path="/categories" element={<h2 className="text-2xl">Categories Page</h2>} />
+              <Route path="/wishlist" element={<h2 className="text-2xl">Wishlist Page</h2>} />
+
+              {/* 404 Route */}
+              <Route path="*" element={<h2 className="text-2xl">404 - Page Not Found</h2>} />
+            </Routes>
           </main>
         </div>
-
-      </UserContext.Provider>
-    </>
+      </Router>
+    </UserContext.Provider>
   )
 }
 
-export default App
+export default App;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,17 @@ import { Config } from 'tailwindcss';
 const config: Config = {
     content: ['./src/**/*.{html,ts,tsx,js,jsx}'],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                playfair: ['Playfair Display', 'serif'],
+                garamond: ['EB Garamond', 'serif'],
+                baskerville: ['Libre Baskerville', 'serif'],
+                lora: ['Lora', 'serif'],
+                cormorant: ['Cormorant Garamond', 'serif'],
+                cinzel: ['Cinzel', 'serif'],
+                montserrat: ['Montserrat', 'sans-serif'],
+            },
+        },
     },
     plugins: [],
 };
